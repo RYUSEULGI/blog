1ffc7fa1349776316f1a87be04317f75f36dfad6
RYUSEULGI <rhythm296@naver.com>
2024-01-09 14:38:37 +0900

    feat: jsonld 추가

--- app/(home)/posts/[slug]/page.tsx
+++ app/(home)/posts/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { PostFooter } from '@/components/post/post-footer'
 import { PostHeader } from '@/components/post/post-header'
 import { Post } from '@/interface/post'
 import { getAllPosts } from '@/libs/post'
+import { baseUrl } from '@/libs/utils'
 import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline'
 import { Metadata } from 'next'
 import { notFound } from 'next/navigation'
@@ -48,28 +49,46 @@ export default async function PostDetailPage({
     return notFound()
   }
 
-  return (
-    <article>
-      <PostHeader title={post.title} date={post.date} />
-      <PostBody content={post.content} />
-      <PostFooter tags={post.tags} />
-      <div className="flex flex-col items-center justify-between pt-10 sm:flex-row">
-        {prevPost ? (
-          <LinkHover href={`/posts/${prevPost.slug}`} className="p-3">
-            <ArrowLeftIcon className="w-4 h-4 mr-2" />
-            <p className="font-semibold">{prevPost.title}</p>
-          </LinkHover>
-        ) : (
-          <div />
-        )}
+  const postJsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'BlogPosting',
+    '@id': `${baseUrl}/posts/${post.slug}#BlogPosting`,
+    name: post.title,
+    description: post.description,
+    datePublished: post.date,
+    url: `${baseUrl}/posts/${post.slug}`,
+  }
 
-        {nextPost && (
-          <LinkHover href={`/posts/${nextPost.slug}`} className="p-3">
-            <p className="font-semibold">{nextPost.title}</p>
-            <ArrowRightIcon className="w-4 h-4 ml-2" />
-          </LinkHover>
-        )}
-      </div>
-    </article>
+  return (
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(postJsonLd),
+        }}
+      />
+      <article>
+        <PostHeader title={post.title} date={post.date} />
+        <PostBody content={post.content} />
+        <PostFooter tags={post.tags} />
+        <div className="flex flex-col items-center justify-between pt-10 sm:flex-row">
+          {prevPost ? (
+            <LinkHover href={`/posts/${prevPost.slug}`} className="p-3">
+              <ArrowLeftIcon className="w-4 h-4 mr-2" />
+              <p className="font-semibold">{prevPost.title}</p>
+            </LinkHover>
+          ) : (
+            <div />
+          )}
+
+          {nextPost && (
+            <LinkHover href={`/posts/${nextPost.slug}`} className="p-3">
+              <p className="font-semibold">{nextPost.title}</p>
+              <ArrowRightIcon className="w-4 h-4 ml-2" />
+            </LinkHover>
+          )}
+        </div>
+      </article>
+    </>
   )
 }
