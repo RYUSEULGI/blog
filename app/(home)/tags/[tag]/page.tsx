8f578f3dff5868f2429986760fe53c5556098739
RYUSEULGI <rhythm296@naver.com>
2024-01-09 14:39:20 +0900

    remove: 사용하지 않는 컴포넌트 제거

--- app/(home)/tags/[tag]/page.tsx
+++ app/(home)/tags/[tag]/page.tsx
@@ -1,19 +1,16 @@
 import { Label } from '@/components/label'
-import Layout from '@/components/layouts'
 import Title from '@/components/typography/title'
 
 export default function TagsPage() {
   return (
-    <Layout>
-      <div className="flex flex-col items-start">
-        <Title>Tags</Title>
+    <div className="flex flex-col items-start">
+      <Title>Tags</Title>
 
-        <div className="flex items-center gap-3 flex-wrap">
-          {[...Array(100)].fill(0).map((v) => (
-            <Label key={v}>{v}</Label>
-          ))}
-        </div>
+      <div className="flex items-center gap-3 flex-wrap">
+        {[...Array(100)].fill(0).map((v) => (
+          <Label key={v}>{v}</Label>
+        ))}
       </div>
-    </Layout>
+    </div>
   )
 }
