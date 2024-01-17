1aee6a90fe69b15ff09bd135cbf811a8be10792d
RYUSEULGI <rhythm296@naver.com>
2024-01-17 17:21:49 +0900

    docs: Next.js로 블로그 만들기(2)

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -4,6 +4,7 @@ import { Navbar } from '@/components/layouts/navbar'
 import { ReactNode } from 'react'
 
 import { baseUrl } from '@/libs/utils'
+import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Providers from '../providers'
 
@@ -11,7 +12,7 @@ import '../../styles/globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
+export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: 'skeyyy 개발 블로그',
   description: 'skeyyy 개발 블로그 입니다.',
